ea0dd9c2bdf0dff7e01c5d7d15d76521a8828e7c
TrongDx <xuantrong02032002@gmail.com>
2023-05-25 19:00:25 +0700

    update file

---
+++ AppReadingComic/app/src/main/java/com/example/appreadingcomic/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.appreadingcomic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+import com.example.appreadingcomic.adapter.ComicAdapter;
+import com.example.appreadingcomic.object.Comic;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+GridView gdvListComic;
+ComicAdapter adapter;
+ArrayList<Comic> comicArrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        anhXa();
+        setUp();
+        setClick();
+    }
+    private void init(){
+        comicArrayList = new ArrayList<>();
+        comicArrayList.add(new Comic("Thượng Cổ Thần Văn","Chapter 11","https://st.nettruyento.com/data/comics/184/thuong-co-than-van.jpg"));
+        comicArrayList.add(new Comic("Hỗn Loạn Nhất Lịch Sử","Chapter 12","https://st.nettruyento.com/data/comics/104/hon-loan-nhat-lich-su.jpg"));
+        comicArrayList.add(new Comic("Dạy Hư Đồ Đệ Phản Diện Rồi Phải Làm Sao Đây ?","Chapter 26","https://st.nettruyento.com/data/comics/236/day-hu-do-de-phan-dien-roi-phai-lam-sao-4257.jpg"));
+        comicArrayList.add(new Comic("Thiếu Chủ Ma Giáo Có Thủ Cung Sa","Chapter 33","https://st.nettruyento.com/data/comics/0/thieu-chu-ma-giao-co-thu-cung-sa.jpg"));
+        comicArrayList.add(new Comic("Thượng Cổ Thần Văn","Chapter 11","https://st.nettruyento.com/data/comics/184/thuong-co-than-van.jpg"));
+        comicArrayList.add(new Comic("Hỗn Loạn Nhất Lịch Sử","Chapter 12","https://st.nettruyento.com/data/comics/104/hon-loan-nhat-lich-su.jpg"));
+        comicArrayList.add(new Comic("Dạy Hư Đồ Đệ Phản Diện Rồi Phải Làm Sao Đây ?","Chapter 26","https://st.nettruyento.com/data/comics/236/day-hu-do-de-phan-dien-roi-phai-lam-sao-4257.jpg"));
+        comicArrayList.add(new Comic("Thiếu Chủ Ma Giáo Có Thủ Cung Sa","Chapter 33","https://st.nettruyento.com/data/comics/0/thieu-chu-ma-giao-co-thu-cung-sa.jpg"));
+        comicArrayList.add(new Comic("Thượng Cổ Thần Văn","Chapter 11","https://st.nettruyento.com/data/comics/184/thuong-co-than-van.jpg"));
+        comicArrayList.add(new Comic("Hỗn Loạn Nhất Lịch Sử","Chapter 12","https://st.nettruyento.com/data/comics/104/hon-loan-nhat-lich-su.jpg"));
+        comicArrayList.add(new Comic("Dạy Hư Đồ Đệ Phản Diện Rồi Phải Làm Sao Đây ?","Chapter 26","https://st.nettruyento.com/data/comics/236/day-hu-do-de-phan-dien-roi-phai-lam-sao-4257.jpg"));
+        comicArrayList.add(new Comic("Thiếu Chủ Ma Giáo Có Thủ Cung Sa","Chapter 33","https://st.nettruyento.com/data/comics/0/thieu-chu-ma-giao-co-thu-cung-sa.jpg"));
+
+        adapter = new ComicAdapter(this, 0, comicArrayList);
+    }
+    private void anhXa(){
+        gdvListComic = findViewById(R.id.gdvListComic);
+    }
+    private void setUp(){
+        gdvListComic.setAdapter(adapter);
+    }
+    private void setClick(){}
+}
